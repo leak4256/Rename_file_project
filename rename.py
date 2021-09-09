dc8fd11f05f3c16fc08b32458be3a95fdfcb561b
lea <leak4256@gmail.com>
2021-09-09 18:26:41 +0300

    update rename

--- rename.py
+++ rename.py
@@ -10,18 +10,19 @@ def renameFile(oldName, newName, path):
     :return: True if the action success
     """
     try:
-        isChange = False  # Saves the file name has been changed
+        isChange = False  # Saves if the file name has been changed
         if pathlib.Path(path).exists():  # Validate the path
-            for file in os.listdir(path):  # Goes through all the directories in the destination dictionary
-                if oldName in file:
-                    os.rename(path + '\\' + file , path + '\\' + file.replace(oldName,newName))
+            for file in os.listdir(path):  # Goes through all the directories
+                # Let to get input in the formats: filename / filename.type / .type
+                if file == oldName or file.split('.')[0] == oldName or oldName.startswith('.') and oldName in file:
+                    os.rename(path + '\\' + file , path + '\\' + file.replace(oldName, newName))
                     isChange = True
-
         if isChange:
-            Application.app.ACKlbl.config(text="The name change successfully")
+            Application.app.ACKlbl.config(text=" The name change successfully")
             return True
         else:
             Application.app.ACKlbl.config(text="path/file is not exist")
+            return False
 
     except Exception as e:
         Application.app.ACKlbl.config(text="ERROR")
